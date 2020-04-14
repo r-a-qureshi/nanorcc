6c175c4508238ef569eb9be31d097d4b05a48252
Rehman <rehman.a.qureshi@gmail.com>
2020-04-14 11:18:00 -0400

    changed raw to counts

--- nanorcc/quality_control.py
+++ nanorcc/quality_control.py
@@ -13,41 +13,41 @@ class QualityControl():
         self.binding_density = binding_density
         self.pos_control_linearity = pos_control_linearity
         self.pos_control_detection_limit = pos_control_detection_limit
-    def fov_qc(self,raw):
+    def fov_qc(self,counts):
         # FOV counts
-        fov = raw['FovCounted']/raw['FovCount']
-        raw['FOV QC'] = (fov <= self.fov_pct)
-        return(raw)
-    def binding_density_qc(self,raw):
+        fov = counts['FovCounted']/counts['FovCount']
+        counts['FOV QC'] = (fov <= self.fov_pct)
+        return(counts)
+    def binding_density_qc(self,counts):
         # Binding Density
-        raw['Binding Density QC'] = (raw['BindingDensity'] <= self.binding_density[0])\
-            | (raw['BindingDensity'] >= self.binding_density[1])
-        return(raw)
-    def pos_control_linearity_qc(self,raw):
+        counts['Binding Density QC'] = (counts['BindingDensity'] <= self.binding_density[0])\
+            | (counts['BindingDensity'] >= self.binding_density[1])
+        return(counts)
+    def pos_control_linearity_qc(self,counts):
         # Positive Control Linearity
-        pos_cols = raw.filter(regex='POS_').columns
+        pos_cols = counts.filter(regex='POS_').columns
         pos_conc = pos_cols.str.extract('(\d+\.*\d*)').astype(float).values.reshape(-1)
-        raw['Positive Control Linearity QC'] =\
+        counts['Positive Control Linearity QC'] =\
             (
-                raw[pos_cols].apply(lambda x: pearsonr(x,pos_conc)[0],axis=1)**2\
+                counts[pos_cols].apply(lambda x: pearsonr(x,pos_conc)[0],axis=1)**2\
                 <= self.pos_control_linearity
             )
-        return(raw)
-    def pos_control_detection_limit_qc(self,raw):
+        return(counts)
+    def pos_control_detection_limit_qc(self,counts):
         # Positive Control Limit of Detection
-        mean = raw.filter('^NEG_[A-Z]').mean(axis=1)
-        std = raw.filter('^NEG_[A-Z]').std(axis=1)
-        raw['Positive Control Detection Limit QC'] =\
-            (raw['POS_E(0.5)']) >= (mean + (self.pos_control_detection_limit * std)) 
-        return(raw)
-    def flag_samples(self,raw):
+        mean = counts.filter('^NEG_[A-Z]').mean(axis=1)
+        std = counts.filter('^NEG_[A-Z]').std(axis=1)
+        counts['Positive Control Detection Limit QC'] =\
+            (counts['POS_E(0.5)']) >= (mean + (self.pos_control_detection_limit * std)) 
+        return(counts)
+    def flag_samples(self,counts):
         """Identify samples where QC metrics failed"""
-        raw = self.fov_qc(raw)
-        raw = self.binding_density_qc(raw)
-        raw = self.pos_control_linearity_qc(raw)
-        raw = self.pos_control_detection_limit_qc(raw)
-        return(raw)
-    def drop_failed_qc(self,raw,reindex=False):
+        counts = self.fov_qc(counts)
+        counts = self.binding_density_qc(counts)
+        counts = self.pos_control_linearity_qc(counts)
+        counts = self.pos_control_detection_limit_qc(counts)
+        return(counts)
+    def drop_failed_qc(self,counts,reindex=False):
         """Return a dataframe excluding samples that failed QC."""
         qc_cols = [
             "FOV QC",
@@ -55,9 +55,9 @@ class QualityControl():
             "Positive Control Linearity QC",
             "Positive Control Detection Limit QC",
         ]
-        raw = self.flag_samples(raw).copy()
-        raw = raw.loc[~raw[qc_cols].any(axis=1)]
-        raw.drop(qc_cols,axis=1,inplace=True)
+        counts = self.flag_samples(counts).copy()
+        counts = counts.loc[~counts[qc_cols].any(axis=1)]
+        counts.drop(qc_cols,axis=1,inplace=True)
         if reindex:
-            raw.reset_index(drop=True,inplace=True)
-        return(raw)
+            counts.reset_index(drop=True,inplace=True)
+        return(counts)
