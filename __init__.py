01a86d941a6166f19b731a04c962c9dbaf5c34a1
rehman <djphist@live.com>
2020-06-30 14:05:35 -0400

    changed __init__.py importing

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from nanorcc import nanorcc
--- nanorcc/__init__.py
+++ nanorcc/__init__.py
@@ -1,1 +1,5 @@
-from nanorcc import *
+from nanorcc.nanorcc import quality_control, preprocess, parse, utils
+# from .quality_control import *
+# from .preprocess import *
+# from .parse import *
+# from .utils import *
